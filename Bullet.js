ac65d414d40fed0e23598ac0d778ee6d91a939f6
Caleb Jones <bionicle.dragon@gmail.com>
2014-03-16 22:29:39 -0400

    Added a function to fire bullets Made basic bullet sim

--- Bullet.js
+++ Bullet.js
@@ -1,9 +1,18 @@
 ﻿#pragma strict
 
+var vel : Vector3;
+
 function Start () {
 
 }
 
 function Update () {
-
+	print(transform.position);
+	Debug.DrawRay(transform.position, vel * Time.deltaTime);
+	var hit : RaycastHit;
+	if (Physics.Raycast(transform.position, vel * Time.deltaTime, hit, (vel*Time.deltaTime).magnitude)) {
+		Debug.DrawRay(hit.point, Vector3.up);
+	} else {
+		transform.Translate(vel * Time.deltaTime, Space.World);
+	}
 }
---
+++ BulletSpawner.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+function Start () {
+	BulletSpawner.FireBullet(transform.position, transform.forward);
+}
+
+function Update () {
+	
+}
+
+static var bulletVel : float = 5;
+static var bulletDens : float = 10;
+
+static function FireBullet(pos : Vector3, dir : Vector3) {
+	var bulletObj = new GameObject("bullet");
+	bulletObj.transform.position = pos;
+	var bullet = bulletObj.AddComponent.<Bullet>();
+	bullet.vel = dir * bulletVel;
+}
